6e0bac3434517e248d76d61d5c37448e4cec365b
mateusz58 <matp321@gmail.com>
2020-03-07 22:00:58 +0100

    Initial commit

---
+++ src/main/java/com/pretius/pretius/PretiusApplication.java
@@ -0,0 +1,12 @@
+package com.pretius.pretius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PretiusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PretiusApplication.class, args);
+    }
+}
